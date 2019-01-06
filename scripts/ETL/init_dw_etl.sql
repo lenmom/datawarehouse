81a17b3bbfc0eaf899584069fb15836813b2ed49
lenmom <lenmom@126.com>
2019-01-06 22:15:24 +0800

    Update init_dw_etl.sql
    fix error.

--- scripts/ETL/init_dw_etl.sql
+++ scripts/ETL/init_dw_etl.sql
@@ -1,5 +1,17 @@
 USE dw_sales;
 
+--SemanticException Cartesian products are disabled for safety reasons. If you know what you are doing, 
+--please sethive.strict.checks.cartesian.product to false and that hive.mapred.mode is not set to 'strict' to proceed. 
+--Note that if you may get errors or incorrect results if you make a mistake while using some of the unsafe features.
+set hive.strict.checks.cartesian.product=false;
+set hive.mapred.mode=nonstrict;
+--In order to change the average load for a reducer (in bytes):
+set hive.exec.reducers.bytes.per.reducer=65535000;
+--In order to limit the maximum number of reducers:
+set hive.exec.reducers.max=6;
+--In order to set a constant number of reducers:
+set mapreduce.job.reduces=2;
+
 -- 清空表
 TRUNCATE TABLE dw_sales.dim_customer;
 TRUNCATE TABLE dw_sales.dim_product;
@@ -7,7 +19,7 @@ TRUNCATE TABLE dw_sales.dim_order;
 TRUNCATE TABLE dw_sales.fact_sales_order;
 
 -- 装载客户维度表
-INSERT INTO dw_sales.customer_dim (customer_sk,customer_number,customer_name,customer_street_address,customer_zip_code,customer_city,customer_state,`version`,effective_date,expiry_date)
+INSERT INTO dw_sales.dim_customer (customer_sk,customer_number,customer_name,customer_street_address,customer_zip_code,customer_city,customer_state,`version`,effective_date,expiry_date)
 SELECT
     row_number() over (ORDER BY t1.customer_number) + t2.sk_max,
     t1.customer_number, 
@@ -19,7 +31,7 @@ SELECT
     1,
     '2019-01-03', 
     '2100-01-01'
-FROM ods_sales.ods_ods_customer t1
+FROM ods_sales.ods_customer t1
 CROSS JOIN 
     (SELECT COALESCE(MAX(customer_sk),0) sk_max 
     FROM dw_sales.dim_customer) t2;
@@ -36,7 +48,7 @@ SELECT row_number() over (ORDER BY t1.product_code) + t2.sk_max,
 FROM ods_sales.ods_product t1
 CROSS JOIN
     (SELECT COALESCE(MAX(product_sk),0) sk_max 
-    FROM dw_sales.product_dim) t2;
+    FROM dw_sales.dim_product) t2;
     
 -- 装载订单维度表
 INSERT INTO dw_sales.dim_order(order_sk,order_number,`version`,effective_date,expiry_date)
@@ -50,13 +62,17 @@ CROSS JOIN
     (SELECT COALESCE(MAX(order_sk),0) sk_max 
     FROM dw_sales.dim_order) t2;
     
+set hive.exec.dynamic.partition=true;
+set hive.exec.dynamic.partition.mode=nonstrict;
+
 -- 装载销售订单事实表
-INSERT INTO dw_sales.fact_sales_order()
-SELECT order_sk, 
-    customer_sk, 
-    product_sk, 
-    date_sk, 
-    order_amount
+INSERT INTO dw_sales.fact_sales_order  partition(order_date) 
+SELECT order_sk as order_sk, 
+    customer_sk as customer_sk, 
+    product_sk as product_sk, 
+    date_sk as order_date_sk, 
+    order_amount as order_amount,
+    a.order_date as order_date
 FROM ods_sales.ods_sales_order a
 JOIN dw_sales.dim_order b ON a.order_number = b.order_number
 JOIN dw_sales.dim_customer c ON a.customer_number = c.customer_number
